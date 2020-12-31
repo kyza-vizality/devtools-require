f4524d72fcaf490a6ba1f0a2a83566441f60fcee
Kyza <40003440+Kyza@users.noreply.github.com>
2020-12-31 10:49:31 -0500

    Totally used the CLI and not the website

---
+++ index.js
@@ -0,0 +1,38 @@
+const { Plugin } = require("@vizality/entities");
+
+module.exports = class DevtoolsRequire extends (
+	Plugin
+) {
+	async onStart() {
+		globalThis.vzr = (module = "") => {
+			module = module.trim();
+			// No module? Just return the core.
+			if (module.length === 0)
+				Object.assign(require(`@vizality`), globalThis.vizality);
+			try {
+				// Try to get the module.
+				return require(`@vizality/${module}`);
+			} catch {
+				// If that doesn't exist, get one of the normally exposed submodules.
+				// As a last resort it might be a Node module.
+				return (
+					this.deepValue(
+						Object.assign(require(`@vizality`), globalThis.vizality),
+						module
+					) ?? require(module)
+				);
+			}
+		};
+	}
+
+	onStop() {
+		delete globalThis.vzr;
+	}
+
+	deepValue(obj, path) {
+		for (var i = 0, path = path.split("/"), len = path.length; i < len; i++) {
+			obj = obj[path[i]];
+		}
+		return obj;
+	}
+};
